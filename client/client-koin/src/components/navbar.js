207623c24c7504f5ef8cff7e6c2bdc080386a731
greoandre <gregoriusandre46@gmail.com>
2022-02-16 12:33:54 +0700

    styling layout done

--- client/client-koin/src/components/navbar.js
+++ client/client-koin/src/components/navbar.js
@@ -9,12 +9,16 @@ function NavBar() {
         <Navbar.Brand href="#home">Koin Works</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-          </Nav>
+          <Nav className="me-auto"></Nav>
           <Nav>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
+            <Nav.Link eventKey={2} href="">
+              <button class="btn btn-outline-light" type="button">
+                <i class="bi-cart-fill me-1"></i>
+                Cart
+                <span class="badge bg-dark text-white ms-1 rounded-pill">
+                  4
+                </span>
+              </button>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
